a801466b3ddde8251b1cd92c165b00ff7e49ffd0
Ella <ellacalugay@gmail.com>
2023-05-20 22:45:07 +0800

    Defining a class named Retry and creating non-parametrized constructor

--- calculator_try_again.py
+++ calculator_try_again.py
@@ -3,7 +3,10 @@
 from calculator_program_oop import Calculator
 
 # Define a class named Retry
+class Retry:
 # Create a non-parametrized constructor
+    def __init__(self):
+        self.retry = ""
 # Ask if the user wants to try again or not.
 # If yes, repeat the process.
 # If no, Display “Thank you!” and exit the program.
