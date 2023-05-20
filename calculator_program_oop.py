189c51ed5b0c4bfb927c45e69bb883d657f9d5be
Ella <ellacalugay@gmail.com>
2023-05-20 21:42:07 +0800

    Creating a class named Calculator and non-parametrized constructor

--- calculator_program_oop.py
+++ calculator_program_oop.py
@@ -2,6 +2,13 @@
 
 # Pseudocode
 # Define a class named Calculator to encapsulate calculator functionality
+class Calculator:
+    # Create a non-parametrized constructor
+    def __init__(self):
+        self.operation = ""
+        self.num1 = 0.0
+        self.num2 = 0.0
+        self.result = None
 # Ask the user to choose one of the four math operations 
 # Ask the user for two numbers
 # Perform the calculation based on the operation that the user wants
