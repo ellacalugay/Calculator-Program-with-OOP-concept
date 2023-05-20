1db0091fc251ec0c02fe6fbb1de9504b78e00a1f
Ella <ellacalugay@gmail.com>
2023-05-20 22:34:33 +0800

    Pseudocode

---
+++ calculator_try_again.py
@@ -0,0 +1,8 @@
+# Pseudocode
+# Import the Calculator class from the calculator_program_oop module
+# Define a class named Retry
+# Create a non-parametrized constructor
+# Ask if the user wants to try again or not.
+# If yes, repeat the process.
+# If no, Display “Thank you!” and exit the program.
+# Exit the loop
