0ac1ed93bd7a585094be45f883d700361f82566f
Ella <ellacalugay@gmail.com>
2023-05-20 22:46:21 +0800

    Ask if the user wants to try again or not

--- calculator_try_again.py
+++ calculator_try_again.py
@@ -7,7 +7,12 @@ class Retry:
 # Create a non-parametrized constructor
     def __init__(self):
         self.retry = ""
+
 # Ask if the user wants to try again or not.
+    def ask_retry(self):
+        while True:
+            try_again = input("Do you want to try again? (yes/no): ")
+            
 # If yes, repeat the process.
 # If no, Display “Thank you!” and exit the program.
 # Exit the loop
