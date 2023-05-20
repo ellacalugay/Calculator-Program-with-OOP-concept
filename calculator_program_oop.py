aff79db8c83785339d69c5d0a3718b29d921894d
Ella <ellacalugay@gmail.com>
2023-05-20 21:34:51 +0800

    Pseudocode

--- calculator_program_oop.py
+++ calculator_program_oop.py
@@ -1,1 +1,8 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #7 | Calculator App with OOP concept
+
+# Pseudocode
+# Define a class named Calculator to encapsulate calculator functionality
+# Ask the user to choose one of the four math operations 
+# Ask the user for two numbers
+# Perform the calculation based on the operation that the user wants
+# Display the result
