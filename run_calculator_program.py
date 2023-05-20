2322e6badab8baed846ad57a344f3415727ea21e
Ella <ellacalugay@gmail.com>
2023-05-20 21:51:14 +0800

    Importing Calculator class and creating instances of it

--- run_calculator_program.py
+++ run_calculator_program.py
@@ -1,1 +1,7 @@
 # Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #7 | Calculator App with OOP concept
+
+# Import the Calculator class from the calculator_program_oop module
+from calculator_program_oop import Calculator
+
+# Create instances of the Calculator
+calculator = Calculator()
