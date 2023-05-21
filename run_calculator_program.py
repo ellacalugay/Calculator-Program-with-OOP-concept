a272c9bc61828f73d520f362f9042e8569ae3283
Ella <ellacalugay@gmail.com>
2023-05-21 17:46:32 +0800

    Editing the error in the two numbers that the user will input

--- run_calculator_program.py
+++ run_calculator_program.py
@@ -81,9 +81,9 @@ class CalculatorApp:
     def calculate(self, event=None):
         # Ask the user for the desired operation
         self.calculator.operation = str(self.combo_operation.get())
-
         # Ask the user to input two numbers
-        self.calculator.get_two_numbers()
+        self.calculator.num1 = float(self.entry_num1.get())
+        self.calculator.num2 = float(self.entry_num2.get())
 
         # Perform the operation based on the user's input
         self.calculator.perform_operation()
