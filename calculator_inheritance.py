71a9660efaf7605bec9554420858f63ff12d0d51
Ella <ellacalugay@gmail.com>
2023-06-03 21:42:34 +0800

    Pseudocode

---
+++ calculator_inheritance.py
@@ -0,0 +1,6 @@
+# Ella Lureen C. Calugay | BSCPE 1-5 | Assignment #8 | Calculator App with OOP concept and inheritance
+
+# Pseudocode
+# Create an inheritance from calculator.py
+# Defining the ScientificCalculator class that inherits from the Calculator class
+# Computation for the square root
